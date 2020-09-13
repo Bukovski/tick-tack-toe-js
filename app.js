922eb9cc528a47ed147ef5500b2d707b11534613
Bukovski <kmanankov@bk.ru>
2020-09-13 15:22:00 +0300

    add: check winner on first line only

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 (function () {
+
+	const table = document.getElementById("fields");
+	const fields = table.getElementsByTagName("td");
+	
+	let currentGamer = "X";
+	
+	for (let i = 0; i < fields.length; i++) {
+		fields[ i ].addEventListener("click", tdClick);
+	}
+	
+	function tdClick(event) {
+		this.innerHTML = currentGamer;
+		
+		this.removeEventListener("click", tdClick);
+		
+		checkWinner();
+		
+		if (currentGamer === "X") {
+			currentGamer = "O";
+		} else {
+			currentGamer = "X";
+		}
+	}
+	
+	function checkWinner() {
+		if (
+			fields[ 0 ].innerHTML === fields[ 1 ].innerHTML
+			&& fields[ 0 ].innerHTML === fields[ 2 ].innerHTML
+		) {
+			console.log("Winner", currentGamer)
+		}
+	}
+
+
+
 	console.log("loaded");
 })();
